37b857ba27f4dacfe1d5e7386d662d3885e8f8a4
StephenWeil <stephen.weil@gmail.com>
2017-09-29 20:43:11 -0500

    bug fix - ghost collision detection and starting position, pacman wont start on coin, also added BG color change to distinguish new level from death animation

--- scripts/pacman.js
+++ scripts/pacman.js
@@ -1,6 +1,9 @@
 /* current bugs:
-    pacman starting on top of coin doesnt eat it
-    dynamic created ghosts move 2-3 times then disappear (but still exist)
+
+    things to add:
+    kill the ghosts (property on pacman set to t/f)
+    teleporters walls/holes
+    add background music/sound effects
 */
 
 $(document).ready(function(){
@@ -20,15 +23,19 @@ $(document).ready(function(){
         [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
         [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
     ];
+    // get random numbers - used frequently
+    function getRnd(num) {
+        return Math.floor(Math.random()*num);
+    };
     // create a random world
-    function randomWorld(worldarray) {
-        function getRnd(num) {
-            return Math.floor(Math.random()*num);
-        };
+    function randomWorld(worldarray) {     
         for (var i = 0; i < worldarray.length; i++) {
             for (var j = 0; j < worldarray[i].length; j++) {
                 if (i == 0 || i == worldarray.length-1 || j == 0 || j == worldarray[i].length-1) {
                     worldarray[i][j] = 2;
+                }
+                else if (i == 1 && j == 1) {
+                    worldarray[i][j] = 0;
                 }
                 else {
                     if (worldarray[i-1][j] != 2 && worldarray[i+1][j] != 2 &&worldarray[i][j-1] != 2 &&worldarray[i][j+1] != 2) {
@@ -55,9 +62,16 @@ $(document).ready(function(){
         x: 6,
         y: 3
     }, {
-        x: 4,
-        y: 4
+        x: 8,
+        y: 2
     }];
+    for (var i = 0; i < ghost.length; i++) {
+        ghost[i].x = getRnd(world[0].length/2)+(world[0].length/2)-1;
+        ghost[i].y = getRnd(world.length/2)+(world.length/2)-1;
+    }
+    displayGhost();
+    $('div.ghost').show();
+    
     // create new ghosts
     function addGhost() {
         ghost.length++;
@@ -67,6 +81,7 @@ $(document).ready(function(){
         };
         var ghostdiv = "<div class='ghost' id='" + ghost.length + "'></div>";
         $('body').append(ghostdiv);
+        $('div.ghost').show();
         displayGhost();
     };
     // redraws the game board
@@ -97,7 +112,8 @@ $(document).ready(function(){
     function checkDeath(){
         for (var i = 0; i < ghost.length; i++) {
             if (pacman.x == ghost[i].x && pacman.y == ghost[i].y) {
-                $('#message').text("Ouch! You were killed by a ghost.")            
+                $('#message').text("Ouch! You were killed by a ghost.")
+                $('body').css('background-color', 'red');            
                 $('#pacman').fadeOut(200, function(){
                     pacman.x = 1;
                     pacman.y = 1;
@@ -112,6 +128,7 @@ $(document).ready(function(){
                     else {
                         movePacman();                
                         $('#pacman').fadeIn();
+                        $('body').css('background-color', 'white');
                     }
                 });
             }
@@ -143,16 +160,25 @@ $(document).ready(function(){
     function checkScore() {
         if (pacman.score >= scoretarget) {
             $('#message').text("Congratulations! You beat the level. Time for a new one.");
+            $('body').css('background-color', 'green');
             $('#pacman').fadeOut(200, function() {
-                scoretarget += 1000*level;
+                scoretarget += 1000;
                 level++;
+                cherryIntervalId = setInterval(createCherry, 10250 - level*250);
+                intervalId = setInterval(moveGhost, 760 - level*10);
                 $('#level').text('Level: ' + level);
                 pacman.x = 1;
                 pacman.y = 1;
+                for (var i = 0; i < ghost.length; i++) {
+                    ghost[i].x = getRnd(world[0].length/2)+(world[0].length/2)-1;
+                    ghost[i].y = getRnd(world.length/2)+(world.length/2)-1;
+                    displayGhost();
+                }
                 $('#gamewrap').fadeOut(200, function() {
                     randomWorld(world);
                     createWorld();
                     movePacman();
+                    $('body').css('background-color', 'white');
                     $('#gamewrap').fadeIn(200);
                     $('#pacman').fadeIn(200);
                     addGhost();
@@ -208,23 +234,55 @@ $(document).ready(function(){
     var intervalId;
 
     function ghostInterval() {
-        intervalId = setInterval(moveGhost, 500);
+        intervalId = setInterval(moveGhost, 750);
     };
 
     function moveGhost() {
         for (var i = 0; i < ghost.length; i++) {
             var randomDir = Math.floor(Math.random()*4);
             if (randomDir == 0 && world[ghost[i].y][ghost[i].x+1] != 2) {
-                ghost[i].x += 1;
+                var test = 0;                
+                for (var g = 0; g < ghost.length; g++) {
+                    if (ghost[i].x + 1 == ghost[g].x && ghost[i].y == ghost[g].y) {
+                        test++;
+                    }
+                }
+                if (test == 0) {
+                    ghost[i].x += 1;                    
+                }
             }
             else if (randomDir == 1 && world[ghost[i].y][ghost[i].x-1] != 2) {
-                ghost[i].x -= 1;
+                var test = 0;                
+                for (var g = 0; g < ghost.length; g++) {
+                    if (ghost[i].x - 1 == ghost[g].x && ghost[i].y == ghost[g].y) {
+                        test++;
+                    }
+                }
+                if (test == 0) {
+                    ghost[i].x -= 1;                    
+                }
             }
             else if (randomDir == 2 && world[ghost[i].y+1][ghost[i].x] != 2) {
-                ghost[i].y += 1;
+                var test = 0;                
+                for (var g = 0; g < ghost.length; g++) {
+                    if (ghost[i].x == ghost[g].x && ghost[i].y + 1 == ghost[g].y) {
+                        test++;
+                    }
+                }
+                if (test == 0) {
+                    ghost[i].y += 1;                    
+                }
             }
             else if (randomDir == 3 && world[ghost[i].y-1][ghost[i].x] != 2) {
-                ghost[i].y -= 1;
+                var test = 0;                
+                for (var g = 0; g < ghost.length; g++) {
+                    if (ghost[i].x == ghost[g].x && ghost[i].y - 1 == ghost[g].y) {
+                        test++;
+                    }
+                }
+                if (test == 0) {
+                    ghost[i].y -= 1;                    
+                }
             }
         }
         displayGhost();
